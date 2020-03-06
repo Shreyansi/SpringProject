1212003a918c68c1b5aca1bc291e64f1f4a0868e
sidshreya <kumar.shreyansi@gmail.com>
2020-03-06 11:37:42 -0800

    Added data to spring model

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +16,22 @@ public class HelloWorldController {
 	
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	//new a controller method read form data
+	//add data to the model
+	@RequestMapping("/processFormVersion2")
+	public String letsShoutDude(HttpServletRequest request, Model model)
+	{
+		//read the request param from html form
+		String theName = request.getParameter("studentName");
+		//convert the data all caps
+		theName = theName.toUpperCase();
+		//create message
+		String result = "Yo " + theName;
+		//add message to model
+		model.addAttribute("message", result);
 		return "helloworld";
 	}
 
