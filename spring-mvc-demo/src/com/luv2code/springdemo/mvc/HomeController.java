2b4719a7e5a620a6a2e6300b0028ff7731f59721
sidshreya <kumar.shreyansi@gmail.com>
2020-03-13 01:03:02 -0700

    added a comment

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HomeController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HomeController.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//added a comment
 @Controller
 public class HomeController {
 	@RequestMapping("/")
