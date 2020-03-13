bb59013f6fe16763afcac88713be017424d61f63
sidshreya <kumar.shreyansi@gmail.com>
2020-03-13 01:32:00 -0700

    use form tags in spring mvc

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -10,46 +10,65 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/hello")
 public class HelloWorldController {
-	//need a controller method to show the initial the html form
+
+	// need a controller method to show the initial HTML form
+	
 	@RequestMapping("/showForm")
 	public String showForm() {
 		return "helloworld-form";
 	}
+		
+	// need a controller method to process the HTML form
 	
 	@RequestMapping("/processForm")
 	public String processForm() {
 		return "helloworld";
 	}
 	
-	//new a controller method read form data
-	//add data to the model
-	@RequestMapping("/processFormVersion2")
-	public String letsShoutDude(HttpServletRequest request, Model model)
-	{
-		//read the request param from html form
+	// new a controller method to read form data and
+	// add data to the model
+
+	@RequestMapping("/processFormVersionTwo")	
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		
+		// read the request parameter from the HTML form
 		String theName = request.getParameter("studentName");
-		//convert the data all caps
+		
+		// convert the data to all caps
 		theName = theName.toUpperCase();
-		//create message
-		String result = "Yo " + theName;
-		//add message to model
+		
+		// create the message
+		String result = "Yo! " + theName;
+		
+		// add message to the model
 		model.addAttribute("message", result);
+				
 		return "helloworld";
 	}
 	
-
-	@RequestMapping("/processFormVersion3")
-	public String processFormVersion3(
-			@RequestParam("studentName") String theName,
-			Model model)
-	{
-		//convert the data all caps
+	@RequestMapping("/processFormVersionThree")	
+	public String processFormVersionThree(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+				
+		// convert the data to all caps
 		theName = theName.toUpperCase();
-		//create message
+		
+		// create the message
 		String result = "Hey My Friend from v3! " + theName;
-		//add message to model
+		
+		// add message to the model
 		model.addAttribute("message", result);
+				
 		return "helloworld";
-	}
-
+	}	
+	
+	
+	
 }
+
+
+
+
+
+
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HomeController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HomeController.java
@@ -2,12 +2,12 @@ package com.luv2code.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-//added a comment
+
 @Controller
 public class HomeController {
+
 	@RequestMapping("/")
 	public String showPage() {
 		return "main-menu";
 	}
-
 }
